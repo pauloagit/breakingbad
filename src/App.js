25bf3bedfa4f7009a4197d6e0cec81722c3499b6
Armando Paulo <armando.paulo@walmart.com>
2019-10-11 10:29:25 -0300

    End class 121

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
 import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function App(){
+
+  const[frase, obtenerFrase] = useState({}); 
+
+  const consultarAPI = async () => {
+    const resultado = await axios('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+
+    //console.log(resultado.data[0]);
+
+    // Agregar el resultado de la API al state; (similar a this.setState)
+    obtenerFrase(resultado.data[0]);
+  }
+
+  // consulta a una Rest Api
+useEffect(
+  () => {
+    consultarAPI();
+  }
+,[])
+
+  console.log(frase);
+
   return <p>Hola</p>
 } 
 
